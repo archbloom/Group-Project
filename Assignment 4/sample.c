9bc477f5b965305c6bca7eb22a2a822e728cb068
archbloom <bhushanauthankar@gmail.com>
2015-05-28 17:01:09 +0530

    First commit

---
+++ Assignment 4/sample.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+  if(argc == 2)
+  {
+    printf("Command line arg is : %s\n",argv[1]);
+  }
+  printf("Hi Bhushan !!\n This is the great day\n");
+
+  return 0;
+}
